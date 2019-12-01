3e3833472c9e8e76571ac094b7ed5fa9bba88a31
Mike Woodman <mwoodman@mtire.org>
2019-12-01 08:46:05 -0500

    initial commit plus day 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2019
+
+A basic repository of my solutions to advent of code puzzles.
+
+[AdventOfCode.com](https://adventofcode.com/)
---
+++ day_1/day_1.py
@@ -0,0 +1,21 @@
+def returnFuel1(mass):
+    return (int(mass/3))-2
+
+def returnFuel2(mass):
+    fuel = 0
+    while mass > 5:
+        temp = (int(mass/3)-2)
+        fuel += temp
+        mass = temp
+    return fuel
+
+datafile = 'data.txt'
+with open(datafile) as df:
+   lines = df.readlines()
+
+part1 = [returnFuel1(int(x.strip())) for x in lines] 
+part2 = [returnFuel2(int(x.strip())) for x in lines] 
+
+print("Part 1:", str(sum(part1)))
+print("Part 2:", str(sum(part2)))
+
