95797359b0ebf7d373231907897d488a227e7516
Mike Woodman <mwoodman@mtire.org>
2019-12-10 22:51:22 -0500

    clean up but still not happy with it

--- day_10/p1_2.py
+++ day_10/p1_2.py
@@ -42,7 +42,6 @@ for row in sky:
     y += 1
 
 
-#[13,17]
 part1 = max(lineofsight)
 print("Part 1:", part1)
 
@@ -51,7 +50,6 @@ baseLocation = points[lineofsight.index(max(lineofsight))]
 def getDistance(x1,y1,x2,y2):
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-tests = []
 def getDistancesAngles(x, y, board):
     tempY=0
     tempX=0
@@ -62,37 +60,35 @@ def getDistancesAngles(x, y, board):
             if c == '#':
                 if not(tempX == x and tempY == y):
                     rad = math.atan2(tempY-y, tempX-x)
-                    #tests.append(abs(rad))
                     deg = math.degrees(rad)
                     if deg < 0:
                         deg = 360 + deg
                     deg = (deg + 90)%360
-                    tests.append(deg)
                     distAngle.append([tempX, tempY, getDistance(x,y,tempX,tempY), deg])
             tempX += 1
         tempY += 1
-    return(distAngle)
+    
+    # N:0 E:90.0 S:180 W:270
+    distAngle.sort(key=lambda x: [x[3], x[2]])
+    return distAngle
 
-# N:0 E:90.0 S:180 W:270
-targets = getDistancesAngles(baseLocation[0],baseLocation[1], sky)
-#targets = getDistancesAngles(11,13, sky)
-targets.sort(key=lambda x: [x[3], x[2]])
-
-destroyed = 0
-part2 = 0
-while len(targets) != 0:
-    lastAngle = -1
-    remove = []
-    for target in targets:
-        if target[3] > lastAngle:
-            lastAngle = target[3]
-            remove.append(target)
-            destroyed += 1
-            if destroyed == 200:
-                part2 = target[0]*100+target[1]
-    for r in remove:
-        targets.remove(r)
-
+def getXTargetDestroyed(base, sky, count):
+    targets = getDistancesAngles(base[0],base[1], sky)
+    destroyed = 0
+    while len(targets) != 0:
+        lastAngle = -1
+        remove = []
+        for target in targets:
+            if target[3] > lastAngle:
+                lastAngle = target[3]
+                remove.append(target)
+                destroyed += 1
+                if destroyed == 200:
+                    return target[0]*100+target[1]
+        for r in remove:
+            targets.remove(r)
+    
+part2 = getXTargetDestroyed(baseLocation, sky.copy(), 200)
 print("Part 2:",part2)        
 
 
